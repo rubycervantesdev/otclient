440b5d42d78fdf5c212fb166ceb4484e419f4a34
Leandro <105954887+Codemaufeito@users.noreply.github.com>
2025-12-30 14:19:34 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -9,20 +9,20 @@ Services = {
     --createAccount = "http://localhost/clientcreateaccount.php", --./client_entergame -- createAccount.lua
 }
 
---[[
 Servers_init = {
-    ["http://127.0.0.1/login.php"] = {
-        ["port"] = 80,
-        ["protocol"] = 1320,
+   -- ["http://client.hylianot.com.br/hylian/login.php"] = {
+    ["https://clienteasy.tibiaeasy.com.br/login.php"] = {
+	
+        ["port"] = 443,
+        ["protocol"] = 1511,
         ["httpLogin"] = true
     },
-    ["ip.net"] = {
-        ["port"] = 7171,
-        ["protocol"] = 860,
-        ["httpLogin"] = false
-    },
+   -- ["127.0.0.1/login.php"] = {
+      --  ["port"] = 80,
+     --   ["protocol"] = 1503,
+      --  ["httpLogin"] = true
+  --  }
 }
-]]
 
 g_app.setName("OTClient - Redemption");
 g_app.setCompactName("otclient");
@@ -107,7 +107,7 @@ local function loadModules()
     end
 
     -- uncomment the line below so that modules are reloaded when modified. (Note: Use only mod dev)
-    -- g_modules.enableAutoReload()
+     g_modules.enableAutoReload()
 end
 
 -- run updater, must use data.zip
